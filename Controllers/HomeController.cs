68ede1512de34e3a8c08608e9fdf10cfbec8110f
MaicoDevDobes <maico@dobesone.com.br>
2022-10-19 12:58:12 -0300

    Add Swagger

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_REST_The_Last_Of_Us.Controllers
+{
+   [ApiController]
+   [Route("[controller]")]
+   public class HomeController : ControllerBase
+   {
+      [HttpGet]
+      public IActionResult Get()
+      {
+         return new OkResult();
+      }
+   }
+}
---
+++ Startup.cs
@@ -0,0 +1,64 @@
+using API_REST_The_Last_Of_Us.Src;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace API_REST_The_Last_Of_Us
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddMvc();
+
+            services.AddSwaggerGen(options =>
+            {
+               options.SwaggerDoc("v1", new OpenApiInfo { Title = "The Last Of Us - API", Version = "v1" });
+               options.SchemaFilter<SwaggerExcludeFilter>();
+            });
+
+            services.AddSwaggerGenNewtonsoftSupport();
+      }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+               app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+               options.SwaggerEndpoint("../swagger/v1/swagger.json", "The Last Of Us API v1");
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+               endpoints.MapControllers();
+            });
+      }
+    }
+}
