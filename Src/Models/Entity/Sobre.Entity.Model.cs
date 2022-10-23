f083b2ba2edefc4b586540c2aa52e0d600eb2108
MaiconGlobes <maico-globes@hotmail.com>
2022-10-23 15:55:15 -0300

    COmmit de segurança

--- Src/Models/Entity/Sobre.Entity.Model.cs
+++ Src/Models/Entity/Sobre.Entity.Model.cs
@@ -34,6 +34,30 @@ namespace API_REST_The_Last_Of_Us.Src.Models.Entity
       [JsonPropertyName("Lancamento")]
       public virtual List<Lancamento> Lancamentos { get; set; }
 
+      [Column("Desenvolvedor")]
+      [Required(ErrorMessage = "Propriedade [desenvolvedor] é obrigatório")]
+      [StringLength(60, ErrorMessage = "Propriedade [desenvolvedor] deve ter no máximo 60 caractere")]
+      [JsonPropertyName("desenvolvedor")]
+      public string Desenvolvedor { get; set; }
+
+      [Column("Direito_Autoral")]
+      [Required(ErrorMessage = "Propriedade [direito_autoral] é obrigatório")]
+      [StringLength(60, ErrorMessage = "Propriedade [direito_autoral] deve ter no máximo 60 caractere")]
+      [JsonPropertyName("direito_autoral")]
+      public string Direito_Autoral { get; set; }
+
+      [Column("Perpectiva")]
+      [Required(ErrorMessage = "Propriedade [perpectiva] é obrigatório")]
+      [StringLength(25, ErrorMessage = "Propriedade [perpectiva] deve ter no máximo 25 caractere")]
+      [JsonPropertyName("direito_autoral")]
+      public string Perpectiva { get; set; }
+
+      [Column("Engine")]
+      [Required(ErrorMessage = "Propriedade [engine] é obrigatório")]
+      [StringLength(10, ErrorMessage = "Propriedade [engine] deve ter no máximo 10 caractere")]
+      [JsonPropertyName("engine")]
+      public string Engine { get; set; }
+
       //[JsonPropertyName("Desenvolvedores")]
       // public virtual List<Desenvolvedor> Desenvolvedores { get; set; }
    }
