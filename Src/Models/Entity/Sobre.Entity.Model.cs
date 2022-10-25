8fe7112f1efe658b5325636f335f2da0ad57997b
MaicoDevDobes <maico@dobesone.com.br>
2022-10-24 21:53:11 -0300

    Correção de bug

--- Src/Models/Entity/Sobre.Entity.Model.cs
+++ Src/Models/Entity/Sobre.Entity.Model.cs
@@ -88,7 +88,7 @@ namespace API_REST_The_Last_Of_Us.Src.Models.Entity
 
       [Column("Sinopse", TypeName = "text")]
       [Required(ErrorMessage = "Propriedade [sinopse] é obrigatório")]
-      [JsonPropertyName("descrsinopseicao")]
+      [JsonPropertyName("sinopse")]
       public string Sinopse { get; set; }
    }
 }
