8751792cfe6f1eba72c076700886689f543bb856
MaiconGlobes <maico-globes@hotmail.com>
2023-01-05 22:42:46 -0300

    _

--- Src/Models/SwaggerResponseType/MensagemErros.Dto.Model.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API_REST_The_Last_Of_Us.Src.Models.SwaggerResponseType
-{
-   public class MensagemErros
-   {
-      public byte Codigo { get; set; }
-
-      public string Descricao { get; set; }
-   }
-}
